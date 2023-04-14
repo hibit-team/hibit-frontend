2220ec16ae9f01af896d4d66a224b3d4396c6811
sangjun-Park1208 <sangjun1997@naver.com>
2023-04-14 16:54:34 +0900

    feat: Mainpage 및 Footer 파일 생성

---
+++ src/components/Footer/intex.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as s from "./styles";
+
+const Footer = () => {
+  return (
+    <s.Wrapper>Footer</s.Wrapper>
+  )
+}
+
+export default Footer;
---
+++ src/pages/Main/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as s from "./styles";
+import LayoutTemplate from '../../components/LayoutTemplate';
+
+const MainPage = () => {
+  return (
+    <LayoutTemplate>
+      
+    </LayoutTemplate>
+  )
+}
+
+export default MainPage;
