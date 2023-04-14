ac9d0f3b8b8940e1bb0c3eac9b87047dd7e49216
sangjun-Park1208 <sangjun1997@naver.com>
2023-04-14 14:47:09 +0900

    chore: App.tsx 기본 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainPage from './pages/Main';
+import styled from "@emotion/styled";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Router>
+        <Routes>
+          <Route path='/' element={<MainPage />}/>
+        </Routes>
+      </Router>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  width: 100vw;
+`;
