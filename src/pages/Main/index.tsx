8e5094705d7a3169edc23f9730f9489c044b9443
sangjun-Park1208 <sangjun1997@naver.com>
2023-04-16 16:06:06 +0900

    feat: 구글 소셜 로그인 및 axios 추가

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import * as s from "./styles";
 import LayoutTemplate from '../../components/LayoutTemplate';
+import GoogleLoginButton from '../../components/GoogleLoginButton';
 
 const MainPage = () => {
   return (
     <LayoutTemplate>
-      <s.Wrapper />
+      <s.Wrapper>
+        <GoogleLoginButton />
+      </s.Wrapper>
     </LayoutTemplate>
   )
 }
