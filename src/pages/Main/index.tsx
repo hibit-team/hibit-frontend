4e4889922b037c064469049f3344196ad004466f
sangjun-Park1208 <sangjun1997@naver.com>
2023-05-14 22:46:13 +0900

    refactor: dev 브랜치 merge 및 메인페이지 구현 resolves #10

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,13 @@
 import * as s from "./styles";
 import LayoutTemplate from '../../components/Common/LayoutTemplate';
 import MainTab from '../../components/Main/MainTab';
-import TabImgs from '../../components/Main/TabImgs';
+import useIsMobile from "../../hooks/useIsMobile";
+import { useRecoilState } from "recoil";
+import { toggleNavState } from "../../recoil/atom/ToggleNavState";
+import MoHeaderComponent from "../../components/Main/Mobile/MobileHeader";
+import MobileNavbar from "../../components/Main/Mobile/MobileNavbar";
+import MobileTab from "../../components/Main/Mobile/MobileTab";
+import MobileSlider from "../../components/Main/Mobile/MobileSlider";
 const MainPage = () => {
   const [toggleState,setToggleState] = useRecoilState<boolean>(toggleNavState);
   const onToggle: Function = ()=>{
@@ -11,16 +17,20 @@ const MainPage = () => {
   if(useIsMobile()){
     if(toggleState){
       //on-off구현
-      return <>
-      <MoHeaderComponent onToggle={onToggle} ></MoHeaderComponent>
-      <MobileNavbar></MobileNavbar> 
-      </>
+      return (
+        <>
+          <MoHeaderComponent onToggle={onToggle} ></MoHeaderComponent>
+          <MobileNavbar></MobileNavbar> 
+        </>
+      )
     }
-    return(<>
-    <MoHeaderComponent onToggle={onToggle}></MoHeaderComponent>
-    <MobileTab></MobileTab>
-    <MobileSlider></MobileSlider>
-    </>)
+    return(
+      <>
+        <MoHeaderComponent onToggle={onToggle}></MoHeaderComponent>
+        <MobileTab></MobileTab>
+        <MobileSlider></MobileSlider>
+      </>
+    )
   }
   return (
     <LayoutTemplate>
