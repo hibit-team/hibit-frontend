8042f35f94ce3d3668d5ecabb104a621adba2efa
sangjun-Park1208 <sangjun1997@naver.com>
2023-05-28 22:42:57 +0900

    feat: 임시 Personality 파일 및 Profile Interface 추가

---
+++ src/interfaces/Profile/IProfile.ts
@@ -0,0 +1,11 @@
+export interface IProfile {
+  nickname: string,
+  age: number,
+  gender: number,
+  personality: string[],
+  introduce: string,
+  job?: string,
+  address_sido?: string,
+  address_sigungu?: string,
+  img?: string[] 
+}
