c749a83b74badfa970fcd3d2b6c1a8199cbcfc4e
Jake shim <shjun1411@naver.com>
2023-07-23 23:03:48 +0900

    feat:대댓글 작성하기 api연동

---
+++ src/hooks/MatchingPost/useSecondaryReplyInputMutation.ts
@@ -0,0 +1,30 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import HttpClient from '../../services/HttpClient';
+import { AxiosError } from 'axios';
+
+//대댓글 작성 hook
+interface ISecondaryReplyMutationParams {
+  replyIDX?: number | undefined;
+  userIDX: number | undefined;
+  body?: string;
+}
+export const useSecondaryReplyInputMutation = (replyIDX:number|undefined) => {
+  const queryClient = useQueryClient();
+  const secondaryReplyInputMutationFn = async ({ replyIDX, userIDX, body }:ISecondaryReplyMutationParams) => {
+    try {
+      const path = `/comment/replies/${replyIDX}/${userIDX}`;
+      const res = HttpClient.post(path, body);
+      return res;
+    } catch (e) {
+      console.error(`${replyIDX}번 댓글의 대댓글 작성에 실패했습니다. error : ${(e as AxiosError).message}`);
+    }
+  };
+  return useMutation<string, AxiosError, ISecondaryReplyMutationParams>(secondaryReplyInputMutationFn, {
+    onSettled: () => {
+      queryClient.invalidateQueries(['reply-lists']);
+    },
+    onError: e => {
+      console.error(`${replyIDX}번 댓글의 대댓글 작성에 실패했습니다. error : ${(e as AxiosError).message}`);
+    },
+  });
+};
