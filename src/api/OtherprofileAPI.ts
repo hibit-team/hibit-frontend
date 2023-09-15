54820b69e354002f5b2299a43d888429c02a74b7
Sangjun Park <sangjun1997@naver.com>
2023-09-16 02:58:02 +0900

    feat: 타인 프로필 완료

--- src/api/OtherprofileAPI.ts
+++ src/api/OtherprofileAPI.ts
@@ -1,9 +1,9 @@
 import HttpClient from "../services/HttpClient";
 
 const OtherprofileAPI = {
-  getOtherProfile: async (otherMemberId: number, loginIdx: number) => {
+  getOtherProfile: async (otherMemberId: number) => {
     try {
-      const path = `/api/profiles/other/${otherMemberId}?id=${loginIdx}`;
+      const path = `/api/profiles/other/${otherMemberId}`;
       const response = await HttpClient.get(path);
       return response;
     } catch (e) {
--- src/pages/OtherProfile/styles.ts
+++ src/pages/OtherProfile/styles.ts
@@ -268,11 +268,12 @@ export const NotRegisterContainer = styled.div`
   justify-content: center;
   align-items: center;
   position: absolute;
-  top: 50%;
+  top: 25%;
   left: 50%;
   background-color: white;
   transform: translate(-50%, 50%);
   z-index: 10;
+  background-color: rgba(255, 255, 255, 0.1);
 `;
 
 export const LockImage = styled.img`
@@ -284,6 +285,7 @@ export const RegisterGuideMent = styled.div`
   font-size: 20px;
   font-weight: 500;
   white-space: pre-line;
+  margin-top: 20px;
   /* line-height: 20px; */
 `;
 
@@ -299,7 +301,9 @@ export const RegisterProfileBtn = styled.div`
   width: 260px;
   height: 56px;
   background-color: #804DD3;
+  margin-top: 40px;
   color: white;
+  border-radius: 10px;
   cursor: pointer;
 `;
 
