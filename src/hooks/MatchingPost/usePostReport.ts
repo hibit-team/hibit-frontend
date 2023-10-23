9a93bc3fb730dcd1235734c771ac964b922fc38d
Jake shim <shjun1411@naver.com>
2023-10-23 12:13:06 +0900

    fix:  신고하기 에러 해결

--- src/hooks/MatchingPost/usePostReport.ts
+++ src/hooks/MatchingPost/usePostReport.ts
@@ -12,27 +12,23 @@ export interface IReportBody {
 }
 
 //게시글 및 댓글 신고 요청 훅
-export const usePostReport = (body: IReportBody) => {
+export const usePostReport = (body:IReportBody) => {
   const queryClient = useQueryClient();
-  const matchingArticleLikeMutationFn = async () => {
-    try {
-      const path = `/declaration/report`;
-      const res = HttpClient.post(path, body,{
-        'Content-Type': 'application/json',
-      });
-      return res;
-    } catch (e) {
-      console.error(`게시글 및 유저 신고에 실패했습니다. error : ${(e as AxiosError).message}`);
-    }
-  };
+  const postReportMutationFn = async ()  => {
+    const path = `/declaration/report`; 
+    const res = await HttpClient.post(path,body ,{
+      'Content-Type': 'application/json',
+    });
+    return res.data;
+};
   const navigate = useNavigate();
-  return useMutation<IReportBody, AxiosError>(() => matchingArticleLikeMutationFn(), {
+  return useMutation<IReportBody, AxiosError>( postReportMutationFn, {
     onSuccess: () => {
       if (body.commentIdx) alert('댓글에 대한 신고가 정상적으로 접수되었습니다.');
       else {
         alert('게시글에 대한 신고가 정상적으로 접수되었습니다.');
       }
-      navigate(`/matchPost/${body.postIdx}`)
+      navigate(`/matching`)
       queryClient.invalidateQueries(['post-info']);
     },
     onError: e => {
