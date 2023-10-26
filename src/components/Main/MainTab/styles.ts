4df4335805f273bb361b5379b56e075cf5bafc03
Sangjun Park <sangjun1997@naver.com>
2023-10-26 23:10:38 +0900

    refactor: Footer 클립보드 추가 및 매칭자세히보기 여백 제거

--- src/components/Main/MainTab/styles.ts
+++ src/components/Main/MainTab/styles.ts
@@ -73,18 +73,6 @@ export const LeftTab_2 = styled.div`
   text-align: center;
   color: #797979;
   border-bottom: 2px solid #797979;
-  /* display: flex;
-  justify-content: center;
-  align-items: center;
-  box-sizing: content-box;
-  color: #797979;
-  padding: 18.5px;
-  text-align:center;
-  width:50%;
-  height:100%;
-  line-height:100%;
-  border-bottom: 2px solid #797979;
-  opacity:0.8; 
 `;
 
 export const RightTab_2 = styled.div`
@@ -98,16 +86,4 @@ export const RightTab_2 = styled.div`
   line-height: 100%;
   border-bottom: 3.5px solid #5E1EC7;
   opacity:0.8;
-  /* display: flex;
-  justify-content: center;
-  align-items: center;
-  font-weight: 900;
-  box-sizing: content-box;
-  padding: 18.5px;
-  width:50%;
-  height:100%;
-  line-height:100%;
-  text-align:center;
-  color:#5E1EC7;
-  border-bottom: 3.5px solid #5E1EC7; */
 `;
