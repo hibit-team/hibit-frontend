0f957aaa4dd439254fd2ec499ce02e2f014d4153
Jake shim <shjun1411@naver.com>
2023-10-30 17:39:53 +0900

    refactor: 게시글 좋아요 프로필 등록시 가능하도록 수정

--- src/hooks/MatchingPost/usePostMatchingArticleLikeMutation.ts
+++ src/hooks/MatchingPost/usePostMatchingArticleLikeMutation.ts
@@ -1,12 +1,15 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import HttpClient from '../../services/HttpClient';
 import { AxiosError } from 'axios';
-
+import { useRecoilValue } from 'recoil';
+import { profileRegisteredState } from '../../recoil/atom/LoginInfoState';
 //게시글 좋아요
 export const usePostMatchingArticleLikeMutation = (postIDX: string | undefined) => {
   const queryClient = useQueryClient();
+  const isProfile = useRecoilValue(profileRegisteredState)
   const matchingArticleLikeMutationFn = async (postIDX: string | undefined) => {
     try {
+      if(!isProfile) throw Error; //프로필 없으면 에러
       const path = `/post/${postIDX}/like`;
       const res = HttpClient.get(path);
       return res;
@@ -17,10 +20,6 @@ export const usePostMatchingArticleLikeMutation = (postIDX: string | undefined) 
   return useMutation(matchingArticleLikeMutationFn, {
     onSettled: () => {
       queryClient.invalidateQueries(['post-info']);
-    },
-    onError: e => {
-      console.error(`${postIDX}번 게시글의 좋아요에 실패했습니다. error : ${(e as AxiosError).message}`);
-      return;
     },
   });
 };
